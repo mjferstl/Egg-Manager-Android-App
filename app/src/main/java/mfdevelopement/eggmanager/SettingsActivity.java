194bd9a875fb0e1f0090fad72f16bdd763a76cf3
mjferstl <39524509+mjferstl@users.noreply.github.com>
2020-08-25 16:32:08 +0200

    changed preferences layout

--- app/src/main/java/mfdevelopement/eggmanager/SettingsActivity.java
+++ app/src/main/java/mfdevelopement/eggmanager/SettingsActivity.java
@@ -1,12 +1,17 @@
 package mfdevelopement.eggmanager;
 
 import android.os.Bundle;
+import android.text.InputType;
+import android.util.Log;
 
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.preference.EditTextPreference;
 import androidx.preference.PreferenceFragmentCompat;
 
 public class SettingsActivity extends AppCompatActivity {
+
+    private final String LOG_TAG = "SettingsActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,9 +28,19 @@ public class SettingsActivity extends AppCompatActivity {
     }
 
     public static class SettingsFragment extends PreferenceFragmentCompat {
+
+        private final String LOG_TAG = "SettingsFragment";
+
         @Override
         public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
             setPreferencesFromResource(R.xml.root_preferences, rootKey);
+
+            EditTextPreference editTextPricePerEgg = findPreference(getString(R.string.preferences_key_pricePerEgg));
+            if (editTextPricePerEgg != null) {
+                editTextPricePerEgg.setOnBindEditTextListener(editText -> editText.setInputType(InputType.TYPE_CLASS_NUMBER | InputType.TYPE_NUMBER_FLAG_DECIMAL));
+            } else {
+                Log.d(LOG_TAG, "The EditTextPreference is null. Thus the keyboard layout cannot be changed.");
+            }
         }
     }
 }
